318e229537d45208e3ad1aa8b76daffa22b5868a
amandafbri <42866976+amandafbri@users.noreply.github.com>
2020-07-07 21:21:31 -0300

    Add more information and padding

--- Preprocessing/basic_vectorizing.py
+++ Preprocessing/basic_vectorizing.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import nltk
 import re
@@ -21,15 +22,17 @@ def words_to_int(words: list):
 tokens_list = [word_tokenize(sentence) for sentence in sentences]
 flat_tokens_list = [item for sublist in tokens_list for item in sublist]
 words_dict = words_to_int(flat_tokens_list)
-print(words_dict)
+sequences_nltk = [[words_dict[word] for word in sentence] for sentence in tokens_list]
+print(sequences_nltk)
 
 # But there is this way using Tensorflow
-tokenizer = Tokenizer(num_words=100, filters='', lower=False)
+tokenizer = Tokenizer(num_words=100, filters='', lower=False, oov_token='<OOV>')
 include_punct = '!'
 sentences = [re.sub(r'(['+include_punct+'])', r' \1', text) for text in sentences]
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
-print(word_index)
+sequences_tf = tokenizer.texts_to_sequences(sentences)
+print(sequences_tf)
 
 '''
  But they behave different!
@@ -38,4 +41,15 @@ print(word_index)
 
  The good news is that Tokenizer from Tensorflow filters a lot of stuff.
  With NLTK I needed to create additional functions to handle basic cleaning.
+
+ In case of words outside the learned vocabulary, NLTK is going to raise KeyError.
+ Tensorflow will ignore the word without any warnings, but we can add the 'oov_token' parameter to deal with that.
 '''
+
+'''
+ maxlen will be the biggest sentence
+ if we want to change that (maxlen=5), it's good to set the truncating part (truncating='post')
+ it will change from [[3, 4, 2], [5, 6, 7, 2]] to [[3 4 2 0][5 6 7 2]]
+ '''
+padded = pad_sequences(sequences_tf, padding='post')
+print(padded)
